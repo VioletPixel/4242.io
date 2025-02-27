8d261db603f885d615f7f9097e737dde5e89afee
Justin Michael <6766849+VioletPixel@users.noreply.github.com>
2025-02-27 11:00:48 -0700

    Add GitHub link to footer

--- public/_includes/footer-signature.php
+++ public/_includes/footer-signature.php
@@ -1,1 +1,3 @@
-<p class="signature">Made with 💟 by <a href="&#x6D;&#x61;&#x69;&#x6C;&#x74;&#x6F;&#x3A;&#x6A;&#x75;&#x73;&#x74;&#x69;&#x6E;&#x40;&#x76;&#x69;&#x6F;&#x6C;&#x65;&#x74;&#x70;&#x69;&#x78;&#x65;&#x6C;&#x2E;&#x63;&#x6F;&#x6D;">Justin Michael</a></p>
+<p class="signature">Made with 💟 by <a href="&#x6D;&#x61;&#x69;&#x6C;&#x74;&#x6F;&#x3A;&#x6A;&#x75;&#x73;&#x74;&#x69;&#x6E;&#x40;&#x76;&#x69;&#x6F;&#x6C;&#x65;&#x74;&#x70;&#x69;&#x78;&#x65;&#x6C;&#x2E;&#x63;&#x6F;&#x6D;">Justin Michael</a>.</p>
+
+<p>This site's code is <a href="https://github.com/VioletPixel/4242.io">available on GitHub.</a></p>
